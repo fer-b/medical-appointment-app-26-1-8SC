1d434d59a833ce2b8d4527352724ecc7102521f9
Yahir Canul <yahir.canul.b@gmail.com>
2026-05-20 22:03:20 -0600

    feat: soft deletes implementation

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use App\Observers\OrderObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 #[ObservedBy(OrderObserver::class)]
 class Order extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'client_id',
         'employee_id',
